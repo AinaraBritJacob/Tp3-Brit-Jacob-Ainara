57159fdadfb796faa4844af7af262574beb24734
AinaraBritJacob <166017112+AinaraBritJacob@users.noreply.github.com>
2024-06-27 15:36:12 -0300

    Add files via upload
    Tp3 repeticiones

---
+++ Tp 3 Brit Jacob/index.js
@@ -0,0 +1,23 @@
+//1
+for (let indice = 10; indice > 0 ; indice= indice -1)
+    {
+   console.log(indice)
+}
+
+//2
+for (let indice2 = 15; indice2 >4 ; indice2= indice2 -1)
+    {
+   console.log(indice2)  
+}
+
+//3
+
+for (let indice3 = 1; indice3 <100; indice3= indice3 +2){
+    console.log(indice3)
+}
+
+//4
+for (let indice4 = 7 ; indice4 <71 ; indice4=indice4 + 7) 
+    {
+    console.log(indice4)
+}
